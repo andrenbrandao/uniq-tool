b75a05a077aa00c76c993bc88338537181e22565
André Brandão <andrenbrandao@gmail.com>
2024-08-03 21:28:49 -0300

    Read from stdin

--- uniq_integration_test.sh
+++ uniq_integration_test.sh
@@ -6,6 +6,8 @@ CMD=$1
 INPUT_TXT=./testdata/test.txt
 EXPECTED_TXT=./testdata/expected_output.txt
 
+failed_any=0
+
 # Executes main and compares if the result is expected.
 $CMD $INPUT_TXT > test_output.txt
 if cmp test_output.txt $EXPECTED_TXT
@@ -16,4 +18,36 @@ else
   diff -u --color=always test_output.txt $EXPECTED_TXT
   echo '---' test: FAIL
   exit 1
+fi
+
+# Reads from standard input
+cat $INPUT_TXT | $CMD - > test_output.txt
+if cmp test_output.txt $EXPECTED_TXT
+then
+  echo '---' test: PASS
+else
+  echo '---' output is not the same as expected_output.txt
+  diff -u --color=always test_output.txt $EXPECTED_TXT
+  echo '---' test: FAIL
+  exit 1
+fi
+
+# Reads from standard input without - flag
+cat $INPUT_TXT | $CMD > test_output.txt
+if cmp test_output.txt $EXPECTED_TXT
+then
+  echo '---' test: PASS
+else
+  echo '---' output is not the same as expected_output.txt
+  diff -u --color=always test_output.txt $EXPECTED_TXT
+  echo '---' test: FAIL
+  exit 1
+fi
+
+#########################################################
+if [ $failed_any -eq 0 ]; then
+    echo '***' PASSED ALL TESTS
+else
+    echo '***' FAILED SOME TESTS
+    exit 1
 fi
--- uniq_main.cc
+++ uniq_main.cc
@@ -1,27 +1,37 @@
 #include "absl/flags/parse.h"
 #include "get_unique.h"
+#include <cstring>
 #include <fstream>
 #include <iostream>
 
+void PrintUnique(std::istream &input_stream, std::ostream &output_stream) {
+  std::vector<std::string> lines;
+  std::string str_read{};
+
+  while (std::getline(input_stream, str_read)) {
+    lines.push_back(str_read);
+  }
+
+  for (const std::string &line : GetUnique(lines)) {
+    output_stream << line << std::endl;
+  }
+}
+
 int main(int argc, char **argv) {
   std::vector<char *> args = absl::ParseCommandLine(argc, argv);
-  std::string filename = args[1];
 
+  if (args.size() == 1 || strcmp(args[1], "-") == 0) {
+    PrintUnique(std::cin, std::cout);
+    return 0;
+  }
+
+  std::string filename = args[1];
   std::ifstream file{filename};
   if (!file) {
     std::cerr << "Could not open file " + filename << '\n';
     return 1;
   }
 
-  std::vector<std::string> lines;
-  std::string str_read{};
-  while (std::getline(file, str_read)) {
-    lines.push_back(str_read);
-  }
-
-  for (const std::string &line : GetUnique(lines)) {
-    std::cout << line << std::endl;
-  }
-
+  PrintUnique(file, std::cout);
   return 0;
 }
